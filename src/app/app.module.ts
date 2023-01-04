4b3dbd8e75bed839a45edcb4c6c7cea8627e64a2
Venkatesh <59081163+venky9885@users.noreply.github.com>
2023-01-04 17:03:40 +0530

    intial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule
+    , FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
